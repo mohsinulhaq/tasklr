b37cc5d33bcec11b5d22e0cfea42751709c3fe59
mohsinulhaq <mohammad.mohsin@sprinklr.com>
2017-10-01 22:47:32 +0530

    add card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Task Management';
+  modules = [
+    {name: 'Publishing', description: 'It is a module which helps use to post in multiple channel at once.', members: 1},
+    {name: 'Paid', description: 'It is a module which helps use to post in multiple channel at once.', members: 2},
+    {name: 'Core', description: 'It is a module which helps use to post in multiple channel at once.', members: 3},
+    {name: 'Engagement', description: 'It is a module which helps use to post in multiple channel at once.', members: 4},
+    {name: 'Distributed', description: 'It is a module which helps use to post in multiple channel at once.', members: 5},
+    {name: 'Social Selling', description: 'It is a module which helps use to post in multiple channel at once.', members: 6},
+    {name: 'RTM', description: 'It is a module which helps use to post in multiple channel at once.', members: 7}
+  ];
+
 }
